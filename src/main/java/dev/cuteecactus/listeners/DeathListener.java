5dd4e22e3ec3a7ade5e97e0f0db26552e77f1292
cuteecactus <abudah181@gmail.com>
2026-07-23 07:29:35 +0500

    implemented respawn

--- src/main/java/dev/cuteecactus/listeners/DeathListener.java
+++ src/main/java/dev/cuteecactus/listeners/DeathListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+import dev.cuteecactus.arena.Arena;
 import dev.cuteecactus.ffa.FFAManager;
 import dev.cuteecactus.kits.Kit;
 import dev.cuteecactus.profile.Profile;
@@ -12,25 +13,37 @@ import dev.cuteecactus.profile.ProfileManager;
 
 public class DeathListener implements Listener {
     @EventHandler
-    public void onPlayerDeath (PlayerDeathEvent e) {
-        if (!(e.getEntity() instanceof Player player)) return;
-
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        if (!(e.getEntity() instanceof Player player))
+            return;
+        e.setCancelled(true);
         Profile profile = ProfileManager.get().getProfile(player.getUniqueId());
-        if (profile == null) return;
+        if (profile == null)
+            return;
         Kit kit = profile.getCurrentKit();
-        if (kit == null ) return;
+        if (kit == null)
+            return;
 
-
-        // Prevent Item drop 
         if (kit.getRule("drop-items") == false) {
             e.getDrops().clear();
         }
 
         if (kit.getRule("death-lobby") == true) {
             FFAManager.get().leaveFFA(player);
-        } else {
-            // TODO: Respawn
+            return;
         }
+
+        // e.setShouldPlayDeathAnimation(false);
+        // e.setDeathDuration(0);
+
+        Arena arena = kit.getArena();
+        // Bukkit.getScheduler().runTask(CactusFFA.get(), () -> {
+        // player.respawn();
+        if (arena != null && arena.getSpawn() != null) {
+            player.teleport(arena.getSpawn());
+        }
+        player.getInventory().setContents(kit.getContent());
+        // });
     }
-    
+
 }
